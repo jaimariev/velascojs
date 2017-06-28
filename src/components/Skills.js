ddc41a72e59d91a088bf83335819048515460cf5
jaimariev <jaimarievelasco@ymail.com>
2017-06-28 15:17:03 -0700

    started the education and skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,8 +5,23 @@ import '../index.css'
 export default React.createClass({
   render(){
     return (
-      <div>
-        <h3>Hello, my name is Jaimarie</h3>
+      <div className="mainDiv">
+        <div className="divOne">
+          <div className="eduDiv">
+            <h2>Education</h2>
+          </div>
+          <div className="expDiv">
+            <h2>Work Experience</h2>
+          </div>
+        </div>
+        <div className="divTwo">
+          <div className="skillsDiv">
+            <h2>Skills</h2>
+          </div>
+          <div className="resumeDiv">
+            <h2><a href="">Click here to check out my resume</a></h2>
+          </div>
+        </div>
       </div>
     )
   }
